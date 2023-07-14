9555a7e34bf7a7442855bb8243ad8d778c5a8a77
Stephanie Crispin <stephaniemfon@gmail.com>
2023-07-13 17:37:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * 
- * Description: Prints C to console
- * @parameter: void
- * 
- * Return: Always 0 (Success)
+ * my-function - This is a description
  */
 int main(void)
 {
