09daa0fa891f64c6f73f192afc03209971da9fbb
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-15 15:45:20 +0100

    added 6-size32

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints different data types
+ *
+ * Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+    char a = 'a';
+    printf("Size of a char: %ld byte(s)\n", sizeof(a));
+    printf("Size of an int: %d byte(s)\n", sizeof(int));
+    printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %lld byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %ld byte(s)\n", sizeof(float));
+    return (0);
+    }
