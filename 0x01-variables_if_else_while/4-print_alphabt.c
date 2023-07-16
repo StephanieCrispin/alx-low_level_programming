740e0a44afb719f7969f68c7d5022ecb2ed784cd
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-16 14:08:25 +0100

    added 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,8 +13,8 @@ int main(void)
 	int i;
 
 	for (i = 'a'; i <= 'z'; ++i)
-        if (i != 'q' && i != 'e')
-            putchar(i);
+		if (i != 'q' && i != 'e')
+			putchar(i);
 
 putchar('\n');
 	return (0);
