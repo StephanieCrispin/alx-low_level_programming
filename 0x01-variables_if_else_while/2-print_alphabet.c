e50a38b618e1e0271fff471760f47c1e870a4147
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-16 13:31:57 +0100

    made updates

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Description: A C program that prints the postivity or negativity of a number
- *
- * Return: Always 0 (Success)
+* main - Entry point
+*
+* Description: A C program that prints the postivity or negativity of a number
+*
+* Return: Always 0 (Success)
 *
 */
-
 int main(void){
 	int i;
 	for(i = 'a'; i <= 'z'; ++i){
