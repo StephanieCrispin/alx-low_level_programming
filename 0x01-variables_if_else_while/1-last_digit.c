c38e641b687e3fb16f2a35e5b232b4e7a064d175
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-16 19:02:35 +0100

    added 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,30 +3,33 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: A C program that prints the postivity or negativity of a number
- *
- * Return: Always 0 (Success)
+* main - Entry point
+*
+* Description: A C program that prints the postivity or negativity of a number
+*
+* Return: Always 0 (Success)
 *
 */
 int main(void)
 {
 	int n;
-    int k;
+	int k;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-    if ((n % 10) == 0){
-        k = n % 10;
-        printf("Last digit of %d is %d and is 0\n", n,k);
-    }else if ((n % 10) < 6 && n != 0){
-        k = n % 10;
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
-    }else if ((n % 10) > 5){
-        k = n % 10;
-        printf("Last digit of %d is %d and is greater than 5\n",n,k);
-    }
+	if ((n % 10) == 0)
+	{
+		k = n % 10;
+		printf("Last digit of %d is %d and is 0\n", n, k);
+	}else if ((n % 10) < 6 && n != 0)
+	{
+		k = n % 10;
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
+	}else if ((n % 10) > 5)
+	{
+		k = n % 10;
+		printf("Last digit of %d is %d and is greater than 5\n", n, k);
+	}
 	return (0);
 }
