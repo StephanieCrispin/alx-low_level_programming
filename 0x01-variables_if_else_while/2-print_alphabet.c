47f35402e044b65c3d3d2f710a048093ad408f78
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-16 13:34:06 +0100

    made updates

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,13 @@
 * Return: Always 0 (Success)
 *
 */
-int main(void){
+int main(void)
+{
 	int i;
-	for(i = 'a'; i <= 'z'; ++i){
+
+	for (i = 'a'; i <= 'z'; ++i)
 		putchar(i);
-	}
+
 	putchar('\n');
 	return (0);
 }
