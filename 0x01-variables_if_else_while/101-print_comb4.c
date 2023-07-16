706d85ecde322f2e0027555d592ddb00f1be6258
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-16 22:37:45 +0100

    added 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description - A C program that prints out all posible combinations of single numbers in three places
+*
+*Return: Always return 0
+*/
+
+int main(void)
+{
+    int i,k,j;
+    for (i = '0'; i <= '7'; i++)
+    {
+        for (k = '0'; k <= '8'; k++)
+        {
+                for (j = '0'; j <= '9'; j++)
+                {
+                    if (i < k && k < j)
+                    {
+                    putchar(i);
+                    putchar(k);
+                    putchar(j);
+                        if (i != '7' || i == '7' && k == '8' && j != '9')
+                        {
+                            putchar(',');
+                            putchar(' ');
+                        }
+                    }
+                }
+        }
+    }
+}
