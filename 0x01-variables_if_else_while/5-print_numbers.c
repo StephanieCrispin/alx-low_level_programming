15b7b87c038f83997be8924ef0088591bb97b30a
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-16 14:25:47 +0100

    added 5-printnumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: A C program that prints the numbers in base 10
+*
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	int i;
+
+	for (i =0; i < 10; i++)
+        printf("%d", i);
+
+printf("\n");
+    return (0);
+}
