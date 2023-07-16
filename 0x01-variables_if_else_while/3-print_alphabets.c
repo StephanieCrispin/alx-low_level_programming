80820022899811771337a495cb6cd1a7d61ea55a
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-16 13:42:43 +0100

    added 3-print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: A C program that prints the alphabets in lower an upper case
+*
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	int i;
+    char k;
+
+	for (i = 'a'; i <= 'z'; ++i)
+		putchar(i);
+    for (k = 'A'; k <= 'Z'; ++k)
+        putchar(k);
+
+	putchar('\n');
+	return (0);
+}
