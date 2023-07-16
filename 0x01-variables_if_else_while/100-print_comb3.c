75cddb7b51aa04fa592091c812329304253add06
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-16 21:52:12 +0100

    added 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: A C progam returns all possible combinations of c
+*
+*Return: Always return 0
+*/
+
+int main(void){
+
+	int i, k;
+
+	for (i = '0'; i <= '8'; i++)
+	{
+	    for (k = '1'; k <= '9'; k++)
+		{
+			putchar(i);
+			putchar(k);
+			if (i != '8' || (i == '8' && k != '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
