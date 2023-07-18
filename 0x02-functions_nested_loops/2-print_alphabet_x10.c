fb6d90b99311f555dc281287491e4330cbb4eded
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-18 17:47:30 +0100

    added 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,19 @@
 
 void print_alphabet_x10(void)
 {
-    int j = 0, i;
+	int j = 0, i;
 
-    while (j < 10)
-    {
-        i ='a';
-        while (i <='z' )
-        {
-            _putchar(i);
-            i++;
-        }
-        _putchar('\n');
-        j++;
-    }
+	while (j < 10)
+	{
+		i ='a';
+		
+		while (i <='z' )
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
 
 }
