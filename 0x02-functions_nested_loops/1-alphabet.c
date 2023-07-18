5db944c52622c6c37a1cfdfd500374832ed7cbdf
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-18 16:41:28 +0100

    updates

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,21 +7,9 @@
 *
 *Return: Always 0 success
 */
-void print_alphabet(void);
-
 
 int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-
-void print_alphabet(void)
-{
-    int i;
-
-    for (i = 'a'; i <= 'z'; i++)
-    {
-        _putchar(i);
-    }
 }
