ef9ee3cd2b8b5988ea97f2576716daf23bf7d40c
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-18 17:26:48 +0100

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
-*main - Entry point
+* print _alphabet
 *
-*Description: A C program that prints the alphabets
-*
-*Return: Always 0 success
 */
 void print_alphabet(void)
 {
-    int i;
+	int i;
 
-    for (i = 'a'; i <= 'z'; i++)
-    {
-        _putchar(i);
-    }
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
 _putchar('\n');
 }
