c74861b93d00dbde7ce35010181734cffb716cd6
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-18 17:44:05 +0100

    added 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* void print_alphabet_x10(void) - Prints "alphabest*10"
+*/
+
+void print_alphabet_x10(void)
+{
+    int j = 0, i;
+
+    while (j < 10)
+    {
+        i ='a';
+        while (i < 'z' )
+        {
+            _putchar(i);
+            i++;
+        }
+        _putchar('\n');
+        j++;
+    }
+
+}
