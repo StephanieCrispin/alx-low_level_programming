b30a63fa5b7d4a898f080f6c94123e429a7c1e62
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-18 03:00:38 +0100

    added updates

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ int main(void)
 
 int i, j;
 
-for ( i = 0 ; i <= 99; i ++)
+for (i = 0 ; i <= 99; i++)
 {
-	for ( j = i + 1; j <= 99; j++)
+	for (j = i + 1; j <= 99; j++)
 	{
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
@@ -22,7 +22,7 @@ for ( i = 0 ; i <= 99; i ++)
 		putchar((j / 10) + '0');
 		putchar((j % 10) + '0');
 
-		if ( i < 98 || j < 99)
+		if (i < 98 || j < 99)
 		{
 			putchar(',');
 			putchar(' ');
