8a9a9fc54f9bc19df676ca5aed5a480f32a1300e
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-18 02:48:14 +0100

    added updates

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,26 +10,27 @@
 int main(void)
 {
 int i, k, j;
+
 for (i = '0'; i <= '7'; i++)
 {
 	for (k = '0'; k <= '8'; k++)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-		if (i < k && k < j)
-		{
-		putchar(i);
-		putchar(k);
-		putchar(j);
-		if (i != '7' || (i == '7' && k == '8' && j != '9'))
+			if (i < k && k < j)
 			{
-				putchar(',');
-				putchar(' ');
+			putchar(i);
+			putchar(k);
+			putchar(j);
+				if (i != '7' || (i == '7' && k == '8' && j != '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 			}
-		}
 		}
 	}
 }
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
