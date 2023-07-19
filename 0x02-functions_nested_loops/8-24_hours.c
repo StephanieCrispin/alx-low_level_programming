b17c5437019a3cd218488044652e1fd8cf1c01cb
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-19 11:11:19 +0100

    added 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* 8-24_hours.c -> prints out time
+* Return: Always 0 success
+*/
+
+void jack_bauer(void)
+{
+    int i, j, k;
+
+    i = 0;
+    while(i < 23)
+    {
+        j = 0;
+        while( j < 23)
+        {
+            k = 0;
+            while (k < 59)
+            {
+                putchar((j / 10) + '0');
+                putchar((j % 10) + '0');
+                putchar(':');
+                putchar((k / 10) + '0');
+                putchar((k / 10) + '0');
+                k++;
+            }
+            j++;
+        }
+        _putchar('\n');
+        i++;
+    }
+    return (0);
+}
