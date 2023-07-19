38fe109b367e2c6478529699a532c310970cfc67
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-19 06:30:57 +0100

    added 6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_last_digit -> returns last digit
+*
+*@n: an integer argument
+*
+*Return: return n
+*/
+
+int print_last_digit(int n)
+{
+    n = n % 10;
+    return (n);
+}
