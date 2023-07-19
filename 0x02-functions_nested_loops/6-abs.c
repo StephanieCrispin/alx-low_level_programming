de008d6f1d799736dbaf7302567ab4415ce45a33
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-19 05:59:30 +0100

    added 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_sign -> prints the sign of the alphabet
+*
+*@n: an integer argument
+*
+*Return: return 0, 1 or -1 depending on the condition
+*/
+
+int _abs(int n)
+{
+    if (n < 0){
+        return (n * -1);
+    }
+}
