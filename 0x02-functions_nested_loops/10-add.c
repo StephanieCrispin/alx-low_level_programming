9fa1191ae67c3192a81ff6330f37a6b2ab02ea05
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-20 03:32:31 +0100

    added 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * largest_number - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: adds two integers
+ */
+
+int add(int a, int b)
+{
+    return (a + b);
+}
