136d23bcc5294342a1f89e9c806bc2b80d54bd8c
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-20 18:15:25 +0100

    added 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*
-*Description: A C function that prints two two possible number combinations
-*
-*Return: Always return 0
-*/
+ *main - Entry point
+ *
+ *Description: A C function that prints two two possible number combinations
+ *
+ *Return: Always return 0
+ */
 
 int main(void)
 {
 
-int i, j;
+	int i, j;
 
-for (i = 0 ; i <= 99; i++)
-{
-	for (j = i + 1; j <= 99; j++)
+	for (i = 0; i <= 99; i++)
 	{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		putchar(' ');
-		putchar((j / 10) + '0');
-		putchar((j % 10) + '0');
-
-		if (i < 98 || j < 99)
+		for (j = i + 1; j <= 99; j++)
 		{
-			putchar(',');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i < 98 || j < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
-}
-putchar('\n');
+	putchar('\n');
 	return (0);
 }
