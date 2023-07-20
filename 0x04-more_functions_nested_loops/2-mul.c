361c1b754a09bcb631acdf78b9f3c5b29f0130ed
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-20 18:20:47 +0100

    added 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -> adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: multiplies two integers
+ */
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
