75e4e3fd325b6bb999ddfe7b60ccd97b4d50a052
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-21 03:48:43 +0100

    aded 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,11 +16,11 @@ void print_square(int size)
 
 		for (i = 1; i <= size; i++)
 		{
-			_putchar('\n');
 			for (j = 1; j <= size; j++)
 			{
 				_putchar('#');
 			}
+			_putchar('\n');
 		}
 	}
 }
