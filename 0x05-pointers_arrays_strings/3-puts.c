d7896815ed35f23075f3229b41b050d4ce9c13b8
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-24 18:39:04 +0100

    added 03

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *swap_int -> Updates the value of pointer
+ *@str: A pointer argument
+ *Return: returns 1 or 0 based on argument
+ */
+
+void _puts(char *str)
+{
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		_putchar()
+	}
+}
