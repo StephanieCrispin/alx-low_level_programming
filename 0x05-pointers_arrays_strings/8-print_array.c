4a7cb2e19490150236b34ac6597f25182904ebd5
Stephanie Crispin <stephaniemfon@gmail.com>
2023-07-24 16:09:07 -0700

    added 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *puts2 -> Prints modulus of string
+ *@str: A pointer argument
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= a[n]; i++)
+	{
+		printf("%d, ", a[n]);
+	}
+	printf("\n");
+}
+
