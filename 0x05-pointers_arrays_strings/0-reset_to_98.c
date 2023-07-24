6ab1d3a7877797cf857fc876e177bac41da2d02b
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-24 15:10:34 +0100

    added 0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *reset_to_98 -> Updates the value of pointer
+ *@n: A number argument
+ *Return: returns 1 or 0 based on argument
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
