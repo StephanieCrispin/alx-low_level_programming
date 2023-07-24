561acc4e6e68ecf24679da2b28a8c086c2338817
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-24 19:42:22 +0100

    added 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,10 +7,11 @@
 void rev_string(char *s)
 {
     int i, c = 0;
+    char temp;
 
     for (i = 0; s[i] != '\0'; i++)
         c++;
 
     for (i = c - 1; i >= 0; i--)
-        s[i];
+        _putchar(s[i]);
 }
