16962b1dff38f8040a49d3b5ce3a04c36ed86742
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-24 15:20:04 +0100

    added 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *swap_int -> Updates the value of pointer
+ *@a: A pointer argument
+ *@b: A pointer argument
+ *Return: returns 1 or 0 based on argument
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
