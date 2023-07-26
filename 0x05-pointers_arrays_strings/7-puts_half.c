008fe3de7e483355a7e64e97d466c6c052d58bbb
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-26 03:39:00 +0100

    made further updates

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- *puts2 -> Prints modulus of string
+ *puts_half -> Prints second half of a string
  *@str: A pointer argument
  */
 
 void puts_half(char *str)
 {
-    int length = 0, middle, i;
+	int length = 0, middle, i;
 
-    while (str[length] != '\0')
-        length++;
-    middle = length / 2;
-    if ((length % 2) != 0)
-        middle = middle + 1;
+	while (str[length] != '\0')
+		length++;
+	middle = length / 2;
+	if ((length % 2) != 0)
+		middle = middle + 1;
 
-    for (i = middle; i <= length - 1; i++)
-        _putchar(str[i]);
+	for (i = middle; i <= length - 1; i++)
+		_putchar(str[i]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
