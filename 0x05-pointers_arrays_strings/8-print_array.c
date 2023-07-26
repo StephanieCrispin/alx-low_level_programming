db3b9c2110f6e427f5c979495f01a7f357db9928
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-26 05:24:18 +0100

    updated finally

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,17 +1,22 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
 /**
- *puts2 -> Prints modulus of string
- *@str: A pointer argument
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
+ * @a: array to be printed
+ * @n: number of elements to print
  */
 void print_array(int *a, int n)
 {
-	int i, size = n - 1;
+	int i;
 
-	for (i = 0; i <= size; i++)
-		if (i != size)
-			printf("%d, ", a[i]);
-
-	printf("%d", a[size]);
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
 	printf("\n");
 }
