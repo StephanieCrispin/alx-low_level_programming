86482616471042285daa0107a9d23b92c4a9794f
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-28 02:23:33 +0100

    added 6X0x00

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat -> A function that concatenates strings
+ * @dest: first param
+ * @src: second param
+ * Return: This function returns a character pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    int len = 0, i;
+
+    while (dest[len])
+        len++;
+
+    for (i = 0; src[i] != '\0'; i++)
+    {
+        dest[len] = src[i];
+        len += 1;
+    }
+    dest[len] = '\0';
+    return (dest);
+}
