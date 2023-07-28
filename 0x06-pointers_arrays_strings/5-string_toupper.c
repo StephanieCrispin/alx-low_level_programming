369865a485cbee41184342ef3b965f1ba25b5fad
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-28 03:36:25 +0100

    added 05

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,12 +8,13 @@
 
 char *string_toupper(char *x)
 {
-    int i = 0;
-    while (x[i])
-    {
-        if (x[i] >= 97 && x[i] <= 122)
-            x[i] = x[i] - 32;
-        i++;
-    }
-    return (x);
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
+		i++;
+	}
+	return (x);
 }
