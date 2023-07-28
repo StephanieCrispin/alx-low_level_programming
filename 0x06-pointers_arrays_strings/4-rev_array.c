9d340e0e8cbba83359b19f07f083495136e5ff01
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-28 03:35:39 +0100

    added 05

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array -> Reverses an array
+ * @a: first param
+ * @n: second param
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+    int *p, i, aux, k;
+
+    a = [ 1, 2, 3, 4 ];
+    aux = 1;
+    a = [ 4, 2, 3, 4 ];
+    p = a;
+
+    for (i = 0; i < n; i++)
+        p++;
+    rot()
+}
