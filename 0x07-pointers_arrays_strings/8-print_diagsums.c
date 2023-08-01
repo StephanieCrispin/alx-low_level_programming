68a4cf7608d259884fa5538cd1599901f132155f
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-01 22:47:25 +0100

    updates

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,14 +11,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-    int l = 0, num1 = 0, num2 = 0;
+	int l = 0, num1 = 0, num2 = 0;
 
-    while (l < size)
-    {
-        num1 += a[(size + 1) * l];
-        num2 += a[(size - 1) * (l + 1)];
-        l++;
-    }
+	while (l < size)
+	{
+		num1 += a[(size + 1) * l];
+		num2 += a[(size - 1) * (l + 1)];
+		l++;
+	}
 
-    printf("%d, %d\n", num1, num2);
+	printf("%d, %d\n", num1, num2);
 }
