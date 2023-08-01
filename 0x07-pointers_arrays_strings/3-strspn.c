9226794698143caa318dc26b41ae9aa9719dc175
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-01 05:54:21 +0100

    added 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn -> prints a buffer in hex
+ * @s: First
+ * @accept: Second
+ * Return: On success int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	strspn(s, accept);
+	return (s);
+}
