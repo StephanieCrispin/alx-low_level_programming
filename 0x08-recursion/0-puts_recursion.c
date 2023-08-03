ccf15fd0f9ea54dd892fff4009adb752405cde6e
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-03 11:37:44 +0100

    added recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+ * _puts_recursion -> prints a string and new line
+ */
+
+void _puts_recursion(char *s)
+{
+    if (!s)
+        _putchar('\n');
+    _putchar(s[0]);
+    _puts_recursion(s[1]);
+}
