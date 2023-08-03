44863b13ade52b9cf7ce137f926246abe9c326d1
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-03 12:45:52 +0100

    updated 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,17 @@
 #include "main.h"
 /**
  * _puts_recursion -> prints a string and new line
+ * @s: Rirst Param
  */
 
 void _puts_recursion(char *s)
 {
-    if (sizeof s <= 0)
-        _putchar('\n');
-    _putchar(s[0]);
-    _puts_recursion(s + 1);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
