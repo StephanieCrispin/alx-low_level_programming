ad2d0095bf0e61b333b7daff1675091e40c78b93
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-03 14:04:59 +0100

    added 1

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ *_strlen_recursion -> Prints out a string length
+ */
+
+int _strlen_recursion(char *s)
+{
+    int length;
+
+    if (*s != '\0')
+    {
+        s++;
+        length++;
+        _strlen_recursion(s);
+    }
+    return (length);
+}
