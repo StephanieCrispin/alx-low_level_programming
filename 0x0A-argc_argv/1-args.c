f6828aa61b68c3ff630b9fbd7be5437158752a63
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-06 10:00:49 +0100

    added 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Main Entry
+ * @argc: First param
+ * Return: returns 0
+ */
+
+int main(int argc)
+{
+	int i;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
