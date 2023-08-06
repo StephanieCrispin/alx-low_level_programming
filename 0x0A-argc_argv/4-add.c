a758585696e31f615295c1f68cfa78cca008d9d5
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-06 17:24:04 +0100

    updated 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,25 +11,21 @@
 
 int main(int argc, char *argv[])
 {
-	int result = 0, num, i, j, k;
+	int i, result = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (!isdigit(*argv[i]))
 		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
-			{
-				printf("%s\n", "Error");
-				return (1);
-			}
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			result = result + atoi(argv[i]);
 		}
 	}
 
-	for (k = 1; k < argc; k++)
-	{
-		num = atoi(argv[k]);
-		result += num;
-	}
 	printf("%d\n", result);
 	return (0);
 }
