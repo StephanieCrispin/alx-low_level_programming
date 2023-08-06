f1fdaf30ec42b624cf05cc8d1bcbf0d62541d6bf
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-06 10:12:20 +0100

    updated 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,26 @@
 #include "main.h"
 #include <stdio.h>
-
+void print_number_of_arguments(int argc);
 /**
  * main - Main Entry
  * @argc: First param
  * Return: returns 0
  */
 
-int main(int argc)
+int main(int argc, char *argv[])
 {
-	int i;
+	(void)argv;
 
-	printf("%d\n", argc - 1);
-
+	print_number_of_arguments(argc);
 	return (0);
+}
+
+/**
+ * print_number_of_arguments - Main Entry
+ * @argc: First param
+ */
+
+void print_number_of_arguments(int argc)
+{
+	printf("%d\n", argc - 1);
 }
