8599bf9d20c4ea253a3f135edd49f2466c19d684
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-06 17:15:03 +0100

    updated 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+int check_that_numbers_are_positive(int argc, char *argv[]);
 int add_positive_numbers(int argc, char *argv[]);
 /**
  * main - Main Entry
@@ -12,9 +13,25 @@ int add_positive_numbers(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
+	check_that_numbers_are_positive(argc, argv);
 	add_positive_numbers(argc, argv);
 	return (0);
 }
+
+int check_that_numbers_are_positive(int argc, char *argv[])
+{
+	int i, j;
+
+	for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+	return (0);
+}
+
 /**
  * add_positive_numbers - A function that adds positive numbers
  * @argc: First param
@@ -24,19 +41,12 @@ int main(int argc, char *argv[])
 
 int add_positive_numbers(int argc, char *argv[])
 {
-	int i, result = 0;
+	int i, result = 0, num;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(*argv[i]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			result = result + atoi(argv[i]);
-		}
+		num = atoi(argv[i]);
+		result += num;
 	}
 
 	printf("%d\n", result);
