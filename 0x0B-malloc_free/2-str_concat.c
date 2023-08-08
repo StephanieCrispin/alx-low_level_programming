701db4d2f864e295de1ebe41ed580acdbe912311
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-08 20:11:58 +0100

    added 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * str_concat -> A pointer that concatenates two strings
+ * @s1: First Param
+ * @s2: Second Param
+ * Return: a char pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+    int i, len, k, l;
+    char *str;
+
+    k = _strlen(s1);
+    l = _strlen(s2) - 1;
+
+    str = malloc(sizeof(char) * (k + l));
+
+    for (i = 0; s2[i] != '\0'; i++)
+    {
+        s1[k] = s2[i];
+        k++;
+    }
+
+    for (len = 0; i < (k + l); i++)
+        str[len] = s1[len];
+
+    str[len] = '\0';
+
+    return (str);
+
+    free(str);
+}
