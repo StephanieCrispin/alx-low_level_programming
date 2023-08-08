3ab9e832c54b960a4b659f4e1aeb4ac0693194f6
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-08 17:52:49 +0100

    added 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	s = malloc(sizeof(char) * size);
-
-	for (i = 0; i < size; i++)
-		s[i] = c;
 	if (s == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
 
 	return (s);
 }
