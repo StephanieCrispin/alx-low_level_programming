4ee55bd3b0cca12abe4692306ec114885541e62d
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-08 19:02:38 +0100

    hot fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,10 +12,15 @@ char *_strdup(char *str)
 	int i, len = 0;
 	char *s;
 
+	if (str == NULL)
+		return NULL;
+
 	while (str[len] != 0)
 		len++;
 
 	s = malloc(sizeof(char) * (len + 1));
+	if (s == NULL)
+		return (NULL);
 
 	for (i = 0; i < len; i++)
 		s[i] = str[i];
