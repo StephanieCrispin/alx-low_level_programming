fad28bb871cb6330dee5ce30c1d7360a34ca66c9
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-09 01:57:02 +0100

    added 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/***
+ * alloc_grid -> create Pointer to pointers of 2D int array
+ * @width: First Param
+ * @height: Second Param
+ * Return : On success int pointer to pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j, k;
+
+	grid = malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+		return NULL;
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+
+		if (grid[i] == NULL)
+		{
+			for (k = 0; k < i; k++)
+				free(grid[k]);
+
+			free(grid);
+			return NULL;
+		}
+
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
