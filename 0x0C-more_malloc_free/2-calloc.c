fd852cdc939a96cd8ead02561692cb4b70879da6
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-10 19:06:05 +0100

    updated code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
     if (size == 0)
         return (NULL);
 
-    ptr = malloc(size * nmemb);
+    ptr = (void *)malloc(size * nmemb);
 
     if (ptr == NULL)
         return (NULL);
