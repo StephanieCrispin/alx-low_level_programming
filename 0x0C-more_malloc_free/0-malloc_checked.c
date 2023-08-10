23a73712c6b0541ac573faed3d7d2bf2699cb30f
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-10 17:08:21 +0100

    updates

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,14 +3,15 @@
 /**
  * malloc_checked -> Returns function failed exit code on malloc FAIL
  * @b: First Parameter
+ * Return: A pointer
  */
 
 void *malloc_checked(unsigned int b)
 {
-    void *ptr;
+	void *ptr;
 
-    ptr = malloc(b);
-    if (ptr == NULL)
-        exit(98);
-    return ptr;
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
 }
