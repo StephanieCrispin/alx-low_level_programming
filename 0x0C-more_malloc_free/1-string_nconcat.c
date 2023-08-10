80bd114757f5eb674d7f0055a9d511b047ac3212
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-10 18:11:17 +0100

    added 2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * string_nconcat -> Concatenates two strings
+ * @s1: First Param
+ * @s2:
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    int i, len1 = 0, len2 = 0, count;
+    char *str;
+
+    if (s1 == NULL)
+        s1 = "";
+
+    if (s2 == NULL)
+        s2 = "";
+
+    while (s1[len1])
+        len1++;
+
+    while (s2[len2])
+        len2++;
+
+    if (n >= (len2 + 1))
+        n = len2;
+
+    count = len1 + n;
+
+    str = (char *)malloc(count * sizeof(char) + 1);
+
+    if (str == NULL)
+        return NULL;
+
+    for (i = 0; i < count; i++)
+    {
+        if (i < len1)
+            str[i] = s1[i];
+        if (i >= len1)
+        {
+            str[i] = s2[len2];
+            len2 += 1;
+        }
+    }
+    str[i] = '\0';
+    return (str);
+}
