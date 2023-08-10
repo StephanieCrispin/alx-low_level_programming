c35abe433d366b5fc406d937e33f01e3c3512af2
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-10 18:22:54 +0100

    updated code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,47 +3,49 @@
 /**
  * string_nconcat -> Concatenates two strings
  * @s1: First Param
- * @s2:
+ * @s2: Second Param
+ * @n: Third Param
+ * Return: returns a char pointer oh
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i, len1 = 0, len2 = 0, count;
-    char *str;
+	unsigned int i, len1 = 0, len2 = 0, count;
+	char *str;
 
-    if (s1 == NULL)
-        s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-    if (s2 == NULL)
-        s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[len1])
-        len1++;
+	while (s1[len1])
+		len1++;
 
-    while (s2[len2])
-        len2++;
+	while (s2[len2])
+		len2++;
 
-    if (n >= (len2 + 1))
-        n = len2;
+	if (n >= (len2 + 1))
+		n = len2;
 
-    count = len1 + n;
+	count = len1 + n;
 
-    str = (char *)malloc(count * sizeof(char) + 1);
+	str = (char *)malloc(count * sizeof(char) + 1);
 
-    if (str == NULL)
-        return NULL;
+	if (str == NULL)
+		return (NULL);
 
-    len2 = 0;
-    for (i = 0; i < count; i++)
-    {
-        if (i < len1)
-            str[i] = s1[i];
-        if (i >= len1)
-        {
-            str[i] = s2[len2];
-            len2 += 1;
-        }
-    }
-    str[i] = '\0';
-    return (str);
+	len2 = 0;
+	for (i = 0; i < count; i++)
+	{
+		if (i < len1)
+			str[i] = s1[i];
+		if (i >= len1)
+		{
+			str[i] = s2[len2];
+			len2 += 1;
+		}
+	}
+	str[i] = '\0';
+	return (str);
 }
