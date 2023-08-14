7192e39fed5f5fd9b48f4af122db627735ee3f92
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-14 19:50:48 +0100

    created 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+An ALX repository with solutions to 0x0E-structures_typedef problems
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A struct definition for dogs
+ * @name: First struct member
+ * @age: Second struct member
+ * @owner: Third struct definition
+ * Description: A struct for  adding new dog info
+*/
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
