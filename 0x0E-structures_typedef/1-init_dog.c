12f1fb8544581253465daceb5411084495a59b94
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-14 20:25:17 +0100

    added 1-init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog -> will init a variable of type strut dog
+ * @d: First param
+ * @name: Second param
+ * @age: Third param
+ * @owner: Fourth param
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
