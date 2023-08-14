7e3df2375f04e2491f677dfd19ed86df95b8d54a
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-14 16:13:19 +0100

    added 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- Main Entry
+ * Description: prints out my current file name
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
