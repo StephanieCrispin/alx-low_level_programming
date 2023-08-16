f9c51c31e84669f2c01f2fd8e1fed0293ace120f
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-17 00:30:22 +0100

    Added 3-code

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+ * main - My main Entry
+ * Description: carries out calculations in program
+ * from argument from the command line
+ * Return: Returns the result of calculation
+ */
+
+int main(int argc, char *argv[])
+{
+	char operator= argv[2];
+	int num1 = atoi(argv[1]), num2 = atoi(argv[3]), result, (*operation)(int, int);
+
+	if (argc < 4 || argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	operation = get_op_func(operator);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = operation(num1, num2);
+
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,75 @@
+#include "3-calc.h"
+
+/**
+ * op_add -> adds two integers
+ * This function adds two integers
+ * and returns the sum of both integers
+ * @a:First Param
+ * @b: Second Param
+ * Return: Returns an integer result
+ */
+
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * op_sub -> subracts two integers
+ * This function will subtract two integers
+ * and return the difference between the two
+ * @a:First Param
+ * @b: Second Param
+ * Return: Returns an integer result
+ */
+
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+/**
+ * op_mul -> multiplies two integers
+ * This function will multiply two integers
+ * @a:First Param
+ * @b: Second Param
+ * Return: Returns an integer result
+ */
+
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * op_div -> will divide two integers
+ * This function divides two integers
+ * @a:First Param
+ * @b: Second Param
+ * Return: Returns an integer result
+ */
+
+int op_div(int a, int b)
+{
+    if (b != 0)
+        return (a / b);
+    printf("Error\n");
+    exit(100);
+}
+
+/**
+ *op_mod -> will modulus two integers
+ * This function gives back the modulus
+ * result of two integers
+ * @a:First Param
+ * @b: Second Param
+ * Return: Returns an integer result
+ */
+
+int op_mul(int a, int b)
+{
+    if (b != 0)
+        return (a % b);
+
+    printf("Error");
+    exit(100);
+}
