6f793b88a45cfbebe3b03256fabe8dede2bed36d
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-17 00:41:11 +0100

    updated 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -67,9 +67,10 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-    if (b != 0)
-        return (a % b);
-
-    printf("Error");
-    exit(100);
+    if (b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    return (a % b);
 }
