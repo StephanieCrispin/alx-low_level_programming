681b17cb32c4dcbc118d2333faacfb2bc2743ec1
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-16 19:53:17 +0100

    added 2-int_index.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 #include <stddef.h>
-
 /**
  * array_iterator -> Iterates an array and acts on each element
  * @array: First Param
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * int_index-> Returns index of first int it finds
+ * @array: array of integers
+ * @size: size of the array
+ * @cmp: the function that compares array elements
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    int i;
+
+    if (array == NULL || cmp == NULL)
+        return (2);
+
+    if (size <= 0)
+        return -1;
+
+    for (i = 0; i < size; i++)
+    {
+        if (cmp(array[i]) != 0)
+        {
+            return i;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    return (0);
+}
