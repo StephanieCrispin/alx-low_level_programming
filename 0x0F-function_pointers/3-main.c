2f7abb23edae28c51b41ab7cc2a75b63e7e11011
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-17 00:47:14 +0100

    updated 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,15 +9,15 @@
 
 int main(int argc, char *argv[])
 {
+	int num1, num2, result, (*operation)(int, int);
 
 	if (argc < 4 || argc > 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	int num1 = atoi(argv[1]), num2 = atoi(argv[3]), result, (*operation)(int, int);
-
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	operation = get_op_func(argv[2]);
 
 	if (operation == NULL)
