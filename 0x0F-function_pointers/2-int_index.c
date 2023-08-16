b324742f33685575ff1722f0162921ae983fde74
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-16 20:01:24 +0100

    Updated 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,26 +11,18 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
+	int i;
 
-    if (array == NULL || cmp == NULL)
-        return (2);
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
 
-    if (size <= 0)
-        return -1;
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]) != 0)
-        {
-
-            return i;
-            break;
-        }
-        else
-        {
-            return -1;
-        }
-    }
-    return (0);
+			return i;
+		}
+	}
+	return (-1);
 }
