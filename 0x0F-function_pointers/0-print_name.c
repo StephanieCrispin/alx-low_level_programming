62b3c7267569db6becc13214af8ac627dfa31a02
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-16 18:16:38 +0100

    added 0x0F-function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name-> A function that will print a name
+ * @name: First param
+ * @f: Second param
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+    (*f)(name);
+}
