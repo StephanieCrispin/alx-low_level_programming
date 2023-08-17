438e3de249b269022203055acf74198e42a6d852
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-17 16:26:19 +0100

    added 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		string = va_arg(stringArguments, char *);
 		if (string == NULL)
-			printf("nil");
+			printf("(nil)");
 		printf("%s", string);
 		if (l != (n - 1) && separator != NULL)
 			printf("%s", separator);
