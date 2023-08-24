fe4f2e0cf45e1beedf44e3126041886083e7193f
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-24 18:54:44 +0100

    added things

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,19 +2,19 @@
 
 size_t print_list(const list_t *h)
 {
-	int y = 0;
+	size_t y = 0;
 	const list_t *pointer = NULL;
 
 	if (h == NULL)
 		printf("Linked list is empty");
-
 	pointer = h;
 
 	while (pointer != NULL)
 	{
 		if (!h->str)
-			printf("([0] nil)\n");
-		printf("[%u] %s\n", pointer->len, pointer->str);
+			printf("([0] (nil)\n");
+		else
+			printf("[%u] %s\n", pointer->len, pointer->str);
 		pointer = pointer->next;
 		y++;
 	}
