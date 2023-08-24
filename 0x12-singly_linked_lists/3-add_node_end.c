9009b12d3b7c9edd21388b77cdfa9bc0eef09f9b
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-25 00:11:17 +0100

    added 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,11 +7,12 @@ list_t *add_node_end(list_t **head, const char *str)
     list_t *pointer = *head;
     unsigned int len;
 
+    len = 0;
     while (str[len])
         len++;
 
     tempValue = malloc(sizeof(list_t));
-    if (tempValue == NULL)
+    if (!tempValue)
         return (NULL);
     tempValue->len = len;
     tempValue->str = strdup(str);
