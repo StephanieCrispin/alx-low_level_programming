6378c972a3e753c96138de07549cc506236c3db8
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-24 20:37:10 +0100

    added 2

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,11 +3,13 @@
 void free_list(list_t *head)
 {
     list_t *tempValue;
-    tempValue = head;
 
+    tempValue = head;
     while (tempValue)
     {
         tempValue = tempValue->next;
+        free(tempValue->str);
         free(tempValue);
+        head = tempValue;
     }
 }
