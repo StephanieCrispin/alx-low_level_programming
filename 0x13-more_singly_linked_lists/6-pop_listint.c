716b9411f5a81579aa4493167d29d1f2ac33f4d0
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-29 22:29:43 +0100

    Made updates

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,10 +1,17 @@
 #include "lists.h"
 
+/**
+ * pop_listint - Its supposed the node at a paticular index and return its value
+ * @head: One and only parameter
+ * Return: returns an integer
+ */
+
 int pop_listint(listint_t **head)
 {
 	listint_t *holdHead;
 
 	int number;
+
 	if (*head == NULL || head == NULL)
 		return (0);
 	number = (*head)->n;
