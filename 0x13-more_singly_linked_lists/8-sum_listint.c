135024cf9ad4a0d9c3cc123b3846e93ef1f8749a
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-29 22:51:07 +0100

    Made updates

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * sum_listint - Sums all the values in our list
+ * @head: Only param
+ * Return: An int, it returns an int
+ */
+
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
