1cdf52a3cf644d016efeb16551fe33fe31b4d15c
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-29 13:50:58 +0100

    updated 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,12 +3,12 @@
 size_t print_listint(const listint_t *h)
 {
     size_t num = 0;
-    const listint_t *ptr = NULL;
-    ptr = h;
-    while (ptr != NULL)
+    const listint_t *ptr = h;
+
+    while (ptr)
     {
-        printf("%d", ptr->n);
-        num++;
+        printf("%d\n", ptr->n);
+        num += 1;
         ptr = ptr->next;
     }
     return (num);
