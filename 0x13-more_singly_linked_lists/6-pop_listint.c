bd78a465af67dd35f0a9e5a308432d299d0bd438
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-29 16:00:53 +0100

    updated 0

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,15 +2,11 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *headPtr;
 	listint_t *holdHead;
+
 	int number;
 	if (*head == NULL || head == NULL)
 		return (0);
-	headPtr = malloc(sizeof(listint_t));
-	if (headPtr == NULL)
-		return (0);
-
 	number = (*head)->n;
 	holdHead = (*head)->next;
 	free(*head);
