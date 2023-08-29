13cefd001b11d8566cb81fb45947a7e5a2998865
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-29 13:53:05 +0100

    updated 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,19 @@
 #include "lists.h"
-
+/**
+ * print_listint-> prints out a list of node values
+ * @h: Only parameter in func
+ * Return: returns the size of the list
+ */
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
-    const listint_t *ptr = h;
+	size_t num = 0;
+	const listint_t *ptr = h;
 
-    while (ptr)
-    {
-        printf("%d\n", ptr->n);
-        num += 1;
-        ptr = ptr->next;
-    }
-    return (num);
+	while (ptr)
+	{
+		printf("%d\n", ptr->n);
+		num += 1;
+		ptr = ptr->next;
+	}
+	return (num);
 }
