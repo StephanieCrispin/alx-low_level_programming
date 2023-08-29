f8bafc451f391c6347abdfa2041ab9e005a5c6d0
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-30 00:25:10 +0100

    Made updates

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,31 +8,31 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    unsigned int j;
-    listint_t *tempValue = *head, *currValue = NULL;
+	unsigned int j;
+	listint_t *tempValue = *head, *currValue = NULL;
 
-    if (*head == NULL)
-        return (-1);
+	if (*head == NULL)
+		return (-1);
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        free(tempValue);
-        return (1);
-    }
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(tempValue);
+		return (1);
+	}
 
-    j = 0;
-    index = index - 1;
-    while (j < index)
-    {
-        if (tempValue == NULL || !(tempValue->next))
-            return (-1);
-        tempValue = tempValue->next;
-        j++;
-    }
+	j = 0;
+	index = index - 1;
+	while (j < index)
+	{
+		if (tempValue == NULL || !(tempValue->next))
+			return (-1);
+		tempValue = tempValue->next;
+		j++;
+	}
 
-    currValue = tempValue->next;
-    tempValue->next = currValue->next;
-    free(currValue);
-    return (1);
+	currValue = tempValue->next;
+	tempValue->next = currValue->next;
+	free(currValue);
+	return (1);
 }
