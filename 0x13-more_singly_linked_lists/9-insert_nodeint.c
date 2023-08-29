128c0ea279815af0f67706d101ba01762f990f5b
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-29 23:26:41 +0100

    Made updates

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,39 +1,46 @@
 #include "lists.h"
 
+/**
+ * insert_nodeint_at_index- Inserts node at a particular index
+ * @head: First param
+ * @idx: Second param
+ * @n: Third param
+ * Return: It returns a node
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *newNode;
-    unsigned int i = 0;
-    listint_t *headPtr = *head;
+	listint_t *newNode;
+	unsigned int i = 0;
+	listint_t *headPtr = *head;
 
-    newNode = malloc(sizeof(listint_t));
-    if (!newNode || head == NULL)
-        return NULL;
+	newNode = malloc(sizeof(listint_t));
+	if (!newNode || head == NULL)
+		return NULL;
 
-    newNode->n = n;
-    newNode->next = NULL;
+	newNode->n = n;
+	newNode->next = NULL;
 
-    if (idx == '\0')
-    {
-        newNode->next = *head;
-        *head = newNode;
-        return (newNode);
-    }
+	if (idx == '\0')
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
 
-    while (headPtr && i < idx)
-    {
+	while (headPtr && i < idx)
+	{
 
-        if (i == idx - 1)
-        {
-            newNode->next = headPtr->next;
-            headPtr->next = newNode;
-            return (newNode);
-        }
-        else
-            headPtr = headPtr->next;
+		if (i == idx - 1)
+		{
+			newNode->next = headPtr->next;
+			headPtr->next = newNode;
+			return (newNode);
+		}
+		else
+			headPtr = headPtr->next;
 
-        i++;
-    }
+		i++;
+	}
 
-    return (NULL);
+	return (NULL);
 }
