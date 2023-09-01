263615667c821350e4e58a14fde59ec409a93c0c
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-01 02:09:16 +0100

    created 14

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - prints out binary representiation of a decimal using bit op
+ * @n: only param
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+	unsigned int temp;
+
+	if (n == '\0')
+	{
+		printf("0");
+		;
+		return;
+	}
+
+	i = 0;
+	temp = n;
+	while ((temp >>= 1) > 0)
+		i++;
+
+	for (; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+	printf("\n");
+}
