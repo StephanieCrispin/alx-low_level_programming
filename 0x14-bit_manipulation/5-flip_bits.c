c1afaa46ff88dd25943f6d48c5eed7ea84a43e84
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-01 03:40:23 +0100

    added another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * flip_bits - flips any bit specified using (XOR)
+ * @n: First param
+ * @m: Second Param
+ * Return: returns an unsigned int
+ */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int keepCount = 0;
