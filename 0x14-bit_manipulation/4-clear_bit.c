8195f23f384cfb951cf959d3d09e3d8a13247d97
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-01 03:31:13 +0100

    added another

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - sets a bit at a given position(index) to 1
+ * clear_bit - sets a bit at a given position(index) to 1
  * @index: Second param
  * @n: First Param
  * Return: returns an int
@@ -9,17 +9,17 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned int hIndex;
-    unsigned long int temp;
+	unsigned int hIndex;
+	unsigned long int temp;
 
-    if (index > sizeof(unsigned long int) * 8)
-        return (-1);
-    hIndex = index;
-    for (temp = 1; hIndex > 0; hIndex--, temp = temp * 2)
-        ;
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	hIndex = index;
+	for (temp = 1; hIndex > 0; hIndex--, temp = temp * 2)
+		;
 
-    if ((*n >> index) & 1)
-        *n = *n - temp;
+	if ((*n >> index) & 1)
+		*n = *n - temp;
 
-    return (1);
+	return (1);
 }
