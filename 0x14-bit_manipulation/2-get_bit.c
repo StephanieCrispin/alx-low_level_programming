11d2f4df14d408843dfaa613eadb545716979c2d
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-01 03:00:16 +0100

    created 14

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * get_bit - Takes out a bit from a particular position in the index and returns it
+ * get_bit - Takes out a bit from a particular
+ * position in the index and returns it
  * @index: Second param
  * @n: First param
  * Return: an int
@@ -13,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int tempVal;
 
 	if (index >= sizeof(unsigned long int) * 8)
-		return -1;
+		return (-1);
 
 	tempVal = n / (1 << index);
 	return (tempVal & 1);
