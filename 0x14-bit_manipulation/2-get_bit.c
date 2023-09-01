ae6943fcc086cf438a6fedcaf0af24919b23db02
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-01 02:59:02 +0100

    created 14

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * get_bit - Takes out a bit from a particular position in the index and returns it
+ * @index: Second param
+ * @n: First param
+ * Return: an int
+ */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int tempVal;
