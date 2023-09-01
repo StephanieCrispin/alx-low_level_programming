7fbe4f2cbaea13095ac3876f32ea0eff4f1e8019
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-01 03:25:37 +0100

    added another

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,12 +9,15 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+    unsigned int hIndex;
     unsigned long int temp;
 
     if (index > sizeof(unsigned long int) * 8)
         return (-1);
-
+    hIndex = index;
     for (temp = 1; index > 0; index--, temp = temp * 2)
+        ;
+    if ((*n >> hIndex) & 1)
         ;
     *n = *n + temp;
 
