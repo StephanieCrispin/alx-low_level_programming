61be4ff7e1a273127980a9de1ceeea101622f805
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-01 03:11:33 +0100

    created 14

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,11 +2,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int tempValue;
+	unsigned long int tempValue;
 
-    if (index > sizeof(unsigned long int) * 8)
-        return (-1);
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
 
-    tempValue = *n >> index;
-    return (tempValue | 1);
+	for (tempValue = 1; index > 0; tempValue = tempValue * 2, index--)
+		*n = *n + tempValue;
+
+	return (1);
 }
