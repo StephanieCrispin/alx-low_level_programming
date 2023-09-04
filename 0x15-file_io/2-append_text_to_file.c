6c97efa7d64ad13d911264216f99a01798fd5382
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-05 00:57:32 +0100

    created file i/o

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text from the text_content
+ *  param to a file
+ * @filename: First param
+ * @text_content: Second param
+ * Reuturn: Returns an int
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int re, wri, len;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content)
+	{
+		for (len = 0; text_content[len] != '\0';)
+			len++;
+	}
+
+	re = open(filename, O_WRONLY | O_APPEND);
+	wri = write(re, text_content, len);
+
+	if (re == -1 || wri == -1)
+		return (-1);
+
+	close(re);
+	return (1);
+}
