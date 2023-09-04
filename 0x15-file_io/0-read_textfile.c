186f84a62a00b0558dcb667120c73d5d92398774
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-05 00:28:32 +0100

    created file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,30 +4,56 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file_descrip, size, output;
-	char *buff;
+
+	ssize_t o, r, w;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	file_descrip = open(filename, O_RDONLY);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	size = read(file_descrip, buff, letters);
-
-	output = write(STDIN_FILENO, buff, size);
-
-	if (file_descrip == -1 || size == -1 || output == -1 || output != size)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buff);
+		free(buffer);
 		return (0);
 	}
 
-	free(buff);
-	close(file_descrip);
+	free(buffer);
+	close(o);
 
-	return (output);
+	return (w);
 }
+
+/*ssize_t file_descrip, size, output;
+char *buff;
+
+if (filename == NULL)
+	return (0);
+
+buff = malloc(sizeof(char) * letters);
+if (buff == NULL)
+	return (0);
+
+file_descrip = open(filename, O_RDONLY);
+
+size = read(file_descrip, buff, letters);
+
+output = write(STDIN_FILENO, buff, size);
+
+if (file_descrip == -1 || size == -1 || output == -1 || output != size)
+{
+	free(buff);
+	return (0);
+}
+
+free(buff);
+close(file_descrip);
+
+return (output);*/
