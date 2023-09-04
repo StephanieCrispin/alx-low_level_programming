17457d4fa38874d8b50bc64f09e5a2b7a85dd15f
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-05 00:04:07 +0100

    created file i/o

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+    int file_descrip, written;
+    int len;
+
+    if (!filename)
+        return (-1);
+
+    if (text_content)
+    {
+        while (text_content[len])
+            len++;
+    }
+
+    file_descrip = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+    written = write(file_descrip, text_content, len);
+
+    if (file_descrip == -1 || written == -1)
+        return (-1);
+
+    close(file_descrip);
+
+    return (1);
+}
