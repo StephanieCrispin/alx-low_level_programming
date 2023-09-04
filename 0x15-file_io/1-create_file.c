962b802994cdf2807f2186afc6499ba370537e02
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-05 00:26:26 +0100

    created file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,6 +4,7 @@
  * create_file - Creates a new file with text from text content
  * @filename: FIrst param
  * @text_content: Second param
+ * Return: -1 or 1
  */
 
 int create_file(const char *filename, char *text_content)
