ddcbca6a927219c0f48614582f477c6dc4ca94f9
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-09-05 00:24:22 +0100

    created file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,27 +1,33 @@
 #include "main.h"
 
+/**
+ * create_file - Creates a new file with text from text content
+ * @filename: FIrst param
+ * @text_content: Second param
+ */
+
 int create_file(const char *filename, char *text_content)
 {
-    int file_descrip, written;
-    int len = 0;
+	int file_descrip, written;
+	int len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content)
-    {
-        while (text_content[len])
-            len++;
-    }
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+	}
 
-    file_descrip = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	file_descrip = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-    written = write(file_descrip, text_content, len);
+	written = write(file_descrip, text_content, len);
 
-    if (file_descrip == -1 || written == -1)
-        return (-1);
+	if (file_descrip == -1 || written == -1)
+		return (-1);
 
-    close(file_descrip);
+	close(file_descrip);
 
-    return (1);
+	return (1);
 }
