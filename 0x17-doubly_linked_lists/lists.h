774e8819947defaff58d6cc5ed273eefc4baeb03
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-13 00:05:08 +0100

    created repo

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -10,7 +10,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
@@ -20,4 +20,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+
 #endif
