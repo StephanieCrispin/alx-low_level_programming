39d1357cb2bbd719afb59038263fd504539d923f
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-13 00:46:45 +0100

    created repo

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+    dlistint_t *newNode, *temporary;
+
+    newNode = malloc(sizeof(dlistint_t));
+    newNode->n = n;
+    newNode->next = NULL;
+
+    if ((*head) == NULL)
+    {
+        newNode->prev = NULL;
+        *head = newNode;
+        return (newNode);
+    }
+
+    temporary = *head;
+
+    while (temporary->next != NULL)
+    {
+        temporary = temporary->next;
+    }
+    temporary->next = newNode;
+    newNode->prev = temporary;
+
+    return (newNode);
+}
