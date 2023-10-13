9c2cb0380709d3412531b319c13fc240ff787700
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-13 01:41:54 +0100

    created repo

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -2,20 +2,20 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-
+    /*dlistint_t *temporary = head;*/
     unsigned int count = 0;
 
     if (head == NULL)
         return (NULL);
-
+    /*I honestly dont know the reasonf for the assignment*/
+    /* temporary->n = head->n;*/
     while (head)
     {
+        if (count == index)
+            return (head);
         head = head->next;
         count += 1;
-        if (count == index)
-            break;
     }
-    if (index > count)
-        return (NULL);
-    return (head);
+
+    return (NULL);
 }
