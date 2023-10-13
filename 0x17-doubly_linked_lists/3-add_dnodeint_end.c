f185102d99abf4781f934f406c93271d30bb81f6
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-13 01:04:45 +0100

    created repo

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-/*dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
     dlistint_t *newNode;
     dlistint_t *temporary;
@@ -26,35 +26,4 @@
     newNode->prev = temporary;
 
     return (newNode);
-}*/
-
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
-{
-    dlistint_t *last_node;
-    dlistint_t *addrs_new_node;
-
-    addrs_new_node = malloc(sizeof(dlistint_t));
-    if (addrs_new_node == NULL)
-        return (NULL);
-    /* add data to addrs_new_node */
-    addrs_new_node->next = NULL;
-    addrs_new_node->n = n;
-    /* make last_node the head to traverse the list till the end */
-    last_node = *head;
-    if (last_node == NULL)
-    {
-        /* if the list is null */
-        /* addrs_new_node becomes the head of the list*/
-        addrs_new_node->prev = NULL;
-        *head = addrs_new_node;
-        return (addrs_new_node);
-    }
-    /* else, traverse till last node */
-    while (last_node->next != NULL)
-        last_node = last_node->next;
-    /* when we get the end of the list change next of last_node*/
-    /* with the new_node */
-    last_node->next = addrs_new_node;
-    addrs_new_node->prev = last_node;
-    return (addrs_new_node);
 }
