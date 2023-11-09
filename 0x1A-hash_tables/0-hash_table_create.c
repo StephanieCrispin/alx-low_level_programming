0b09d01170cbc5c494801656d4ac967e765297a4
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-11-09 01:13:34 +0100

    worked on 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+
+    long unsigned int i = 0;
+    hash_table_t *newTable;
     if (!size)
         return (NULL);
-
-    int i = 0;
     /*Creates a new hashtable*/
 
-    hash_table_t *newTable = malloc(sizeof(hash_table_t));
+    newTable = malloc(sizeof(hash_table_t));
+
+    if (!newTable)
+    {
+        return (NULL);
+    }
     newTable->size = size;
     newTable->array = (hash_node_t **)calloc(newTable->size, sizeof(hash_node_t *));
 
