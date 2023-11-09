576eacccb1a3da5f35bd50de6edaa21701d40a07
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-11-09 05:14:23 +0100

    worked on 0

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,29 +1,29 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_get - gets from a hash table
- * @ht: table item to be gotten
- * @key: The key to get it
- * Return: A char pointer argument
+ * hash_table_get - retrieves a val
+ * @ht: is the hash ta
+ * @key: is the key I'm looking fo
+ * Return: the value associated wi
  */
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-	hash_node_t *hashed = NULL;
+	hash_node_t *hash_nodes = NULL;
 
-	if (ht == NULL || !key)
+	if (!ht || !key)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	hashed = ht->array[index];
-
-	for (; hashed != NULL; hashed = hashed->next)
+	/*checks for somethin*/
+	hash_nodes = ht->array[index];
+	while (hash_nodes != NULL)
 	{
-		if (strcmp(hashed->key, key) == 0)
-			return (hashed->value);
+		/* if the passed key is in the hash table*/
+		if (strcmp(hash_nodes->key, key) == 0)
+			return (hash_nodes->value);
 		/* otherwise keeps looking */
-		hashed = hashed->next;
+		/*chacks for something*/
+		hash_nodes = hash_nodes->next;
 	}
 	/* if the index is not found cuz the ley doesn't exists */
 	return (NULL);
