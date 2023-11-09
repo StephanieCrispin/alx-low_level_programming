07f75bacf6ddd412b0ebea715175f38342e7397a
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-11-09 01:38:10 +0100

    worked on 0

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,6 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index = hash_djb2(key);
-    return (*key % size);
+	unsigned long int index = hash_djb2(key);
+	return (*key % size);
 }
-
