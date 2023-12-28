2956fd2307a003c123358548def506d412cae248
StephanieMfon <okpomfonstephanie@gmail.com>
2023-12-28 19:12:12 +0100

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Defines the perimeter of an island."""
+"""Defines an island perimeter measuring function."""
 
-def island_perimeter(grid): 
-  	"""Return the perimiter of an island.
+
+def island_perimeter(grid):
+    """Return the perimiter of an island.
 
     The grid represents water by 0 and land by 1.
 
@@ -11,21 +12,17 @@ def island_perimeter(grid):
     Returns:
         The perimeter of the island defined in grid.
     """
-	perimeter=0
-	rows=len(grid)
-	cols=len(grid[0])
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-	for row in range(rows):
-		for col in range(cols):
-			if grid[row][col] == 1:
-				perimeter += 4
-			if row > 0 and grid[row -1][col] == 1:
-				perimeter -= 1
-			if row < rows - 1 and grid[row + 1] [col] == 1:
-				perimeter -= 1
-			if col > 0 and grid [row][col -1] == 1:
-				perimeter -= 1
-			if col < cols -1 and grid [row] [col + 1] == 1:
-				perimeter -=1
-
-	return perimeter
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
